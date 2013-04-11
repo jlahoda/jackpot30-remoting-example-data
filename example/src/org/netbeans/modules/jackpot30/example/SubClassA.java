41728fe5cec9143356bbf9591204637e7c8cb7e7
Jan Lahoda <lahoda@gmail.com>
2013-04-11 16:08:30 +0200

    Adding example usages

---
+++ example/src/org/netbeans/modules/jackpot30/example/SubClassA.java
@@ -0,0 +1,10 @@
+package org.netbeans.modules.jackpot30.example;
+
+public class SubClassA extends ClassA {
+
+    @Override
+    public void method() {
+        super.method();
+    }
+
+}
---
+++ example/src/org/netbeans/modules/jackpot30/example/UseClassA.java
@@ -0,0 +1,9 @@
+package org.netbeans.modules.jackpot30.example;
+
+public class UseClassA {
+
+    public void user() {
+        ClassA a = new ClassA();
+        a.method();
+    }
+}
